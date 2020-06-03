82e8181133c8b9160ee29d0238a0d9542581697f
Kukkik1998s <43192851+Kukkik1998s@users.noreply.github.com>
2020-06-03 08:37:57 +0700

    pj version2

---
+++ gemini8.0/src/main/java/edu/gemini/app/ocs/MyEx.java
@@ -0,0 +1,13 @@
+package edu.gemini.app.ocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEx {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEx.class, args);
+    }
+
+}
